d2e96ca7d838765be22a822032e85ba8a4f9124a
payalsahare <payalsahare369@gmail.com>
2020-10-16 12:17:58 +0530

    First Commit....

---
+++ src/gitHubDemo/GitHubDemoClass.java
@@ -0,0 +1,15 @@
+package gitHubDemo;
+
+public class GitHubDemoClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String hungry="yes";
+		if(hungry=="yes")
+			System.out.println("Eat Something");
+		else
+			System.out.println("Do Work");
+	}
+
+}
